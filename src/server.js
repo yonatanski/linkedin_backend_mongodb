f71e069b344a55dd8775294f91e1e56e50099fd5
yonatanski <phpcodeyg@gmail.com>
2022-02-21 10:45:19 +0100

    server.js created

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import cors from "cors"
+import mongoose from "mongoose"
+
+const server = express()
+const port = process.env.PORT || 3005
+
+// ************************************* MIDDLEWARES ***************************************.
+
+server.use(cors())
+server.use(express.json())
+
+// ************************************* ROUTES ********************************************
+
+// ************************************** ERROR HANDLERS ***********************************
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!")
+  server.listen(port, () => {
+    console.table(listEndpoints(server))
+    console.log("Server runnning on port: ", port)
+  })
+})
