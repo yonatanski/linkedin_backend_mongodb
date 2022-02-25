0f165dd0c1a8b97fabc7a813f6085af69b8cdf0f
yonatanski <phpcodeyg@gmail.com>
2022-02-25 02:15:36 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,25 +11,27 @@ const port = process.env.PORT || 3001
 
 // ************************************* MIDDLEWARES ***************************************.
 const whiteListOrigins = [process.env.PROD_FE_URL]
-server.use(cors({origin:function(origin, next){
-  if(!origin || whiteListOrigins.indexOf(origin) !== -1) 
-  next(null, true)
-  else 
-  next(new Error("cors error"))
-}}))
+server.use(
+  cors({
+    origin: function (origin, next) {
+      if (!origin || whiteListOrigins.indexOf(origin) !== -1) next(null, true)
+      else next(new Error("cors error"))
+    },
+  })
+)
 server.use(express.json())
 
 // ************************************* ROUTES ********************************************
 server.use("/posts", postRouter)
 
-server.use("/profiles", profilesRouter);
+server.use("/profiles", profilesRouter)
 // ************************************** ERROR HANDLERS ***********************************
 server.use(badRequestHandler)
 server.use(unauthorizedHandler)
 server.use(notFoundHandler)
 server.use(genericErrorHandler)
-
-mongoose.connect(process.env.MONGO_CONNECTION)
+const uri = process.env.MONGODB_URI
+mongoose.connect(uri)
 
 mongoose.connection.on("connected", () => {
   console.log("Successfully connected to Mongo!")
